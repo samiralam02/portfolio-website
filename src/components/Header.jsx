42d53764f098e19aa1dffa7dc4edc97045976391
samiralam02 <sam02samir@gmail.com>
2025-03-07 14:29:07 +0530

    Rebuild project with updated image paths and configuration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
         <h1>
           <a href="/" className="logo">
               <img
-                src="/public/images/logo.svg"
+                src="/images/logo.svg"
                 width={60}
                 height={40}
                 alt="Samir Alam"
